b912ee2f6f209aa77211e89b819ba8810ef70d55
Ben Mechen <benm123@yahoo.com>
2020-12-30 15:54:45 +0000

    Fix syntax

--- src/components/seo.js
+++ src/components/seo.js
@@ -65,10 +65,12 @@ function SEO({ description, lang, meta, keywords, title }) {
                 {
                     name: `twitter:description`,
                     content: metaDescription
-                }, 
+                },
                 {
-                    name="google-site-verification", content="4K5xIf56S8YAEd2PcuJurvvFujwhE97qrmzDuLSl-tw"
-                }]
+                    name: `google-site-verification`,
+                    content: `4K5xIf56S8YAEd2PcuJurvvFujwhE97qrmzDuLSl-tw`
+                }
+            ]
                 .concat(
                     keywords.length > 0
                         ? {
